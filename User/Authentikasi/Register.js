cd0dc5cf49b0a6d02fba62dc6b469fb8cf17b71c
samsul <samsul.marif100@gmail.com>
2022-09-23 22:13:56 +0800

    remote method put && delete

--- User/Authentikasi/Register.js
+++ User/Authentikasi/Register.js
@@ -8,11 +8,3 @@ app.get('/namaRoutes', (res, req) => {
 app.post('/namaroutes', (res, req) => {
     res.send('halo')
 })
-
-app.put('/namaroutes', (res, req) => {
-    res.send('halo mbak')
-})
-
-app.delete('/namaroutes', (req, res) => {
-    res.send('halo')
-})
